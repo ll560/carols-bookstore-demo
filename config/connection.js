6075c83c9158c1a66776796157a24a394bc47bac
ll560 <llavia6@gmail.com>
2021-05-22 15:08:44 -0400

    update for mongodb in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, (err) => {
   if(!err) {
     console.log("Successful connection with MongoDB Server");  
   }
